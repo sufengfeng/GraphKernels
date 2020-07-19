1230cc30f4b1ed7c32a89a13edee1c55fe030cec
sufengfeng <1041168096@qq.om>
2020-07-19 05:08:25 -0700

    format

--- Tutorial/demo_mutag.py
+++ Tutorial/demo_mutag.py
@@ -11,25 +11,24 @@ import numpy as np
 
 # Load data
 # comment next line if data.mutag is in a different folder
-#mutag_list = np.load("graphkernels/data.mutag")
+# mutag_list = np.load("graphkernels/data.mutag")
 
 # Uncomment next line if data.mutag is in your current folder
 mutag_list = np.load("data.mutag")
 
 ### ALL KERNELS COMPUTE
 K1 = gk.CalculateEdgeHistKernel(mutag_list)
-K2 = gk.CalculateVertexHistKernel(mutag_list) 
+K2 = gk.CalculateVertexHistKernel(mutag_list)
 K3 = gk.CalculateVertexEdgeHistKernel(mutag_list)
 K4 = gk.CalculateVertexVertexEdgeHistKernel(mutag_list)
 K5 = gk.CalculateEdgeHistGaussKernel(mutag_list)
 K6 = gk.CalculateVertexHistGaussKernel(mutag_list)
 K7 = gk.CalculateVertexEdgeHistGaussKernel(mutag_list)
-#K8 = gk.CalculateGeometricRandomWalkKernel(mutag_list)
-#K9 = gk.CalculateExponentialRandomWalkKernel(mutag_list)
+# K8 = gk.CalculateGeometricRandomWalkKernel(mutag_list)
+# K9 = gk.CalculateExponentialRandomWalkKernel(mutag_list)
 K10 = gk.CalculateKStepRandomWalkKernel(mutag_list)
 K11 = gk.CalculateWLKernel(mutag_list)
 K12 = gk.CalculateConnectedGraphletKernel(mutag_list, 4)
 K13 = gk.CalculateGraphletKernel(mutag_list, 4)
-#K14 = gk.CalculateShortestPathKernel(mutag_list)
+# K14 = gk.CalculateShortestPathKernel(mutag_list)
 print "debug"
-
