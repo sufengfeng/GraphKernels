c3690e46947509a5427542ffb5eb8548d05ac805
sufengfeng <1041168096@qq.om>
2020-07-20 08:57:38 +0800

    增加新文件

--- Tutorial/demo_mutag.py
+++ Tutorial/demo_mutag.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.7
+# -*- coding:utf-8 -*-
 """
 In this demo we show how to
 calculate the kernel matrices
@@ -16,6 +18,7 @@ import numpy as np
 # Uncomment next line if data.mutag is in your current folder
 mutag_list = np.load("data.mutag")
 
+
 ### ALL KERNELS COMPUTE
 K1 = gk.CalculateEdgeHistKernel(mutag_list)
 K2 = gk.CalculateVertexHistKernel(mutag_list)
@@ -32,3 +35,19 @@ K12 = gk.CalculateConnectedGraphletKernel(mutag_list, 4)
 K13 = gk.CalculateGraphletKernel(mutag_list, 4)
 # K14 = gk.CalculateShortestPathKernel(mutag_list)
 print "debug"
+
+###所有内核计算
+# 计算边缘历史核
+# 计算顶点历史核
+# 计算顶点边缘历史核
+# 计算顶点顶点边缘历史核
+# 计算边缘HistGauss核
+# 计算顶点HistGauss内核
+# 计算顶点边缘HistGauss内核
+# 计算几何随机游走核
+# 计算指数随机游走核
+# 计算KStep随机游走内核
+# 计算WL内核
+# 计算ConnectedGraphlet内核
+# 计算Graphlet内核
+# 计算ShortestPath内核
